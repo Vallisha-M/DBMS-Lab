0f658b3025c8582ffea85f0a8a9277ec9cb2a315
Vallisha M <vallishamohan@gmail.com>
2021-05-06 11:56:33 +0530

    Added query 3

--- Bank/Bank.sql
+++ Bank/Bank.sql
@@ -90,4 +90,9 @@ select * from depositer;
 select * from loan;
 
 delete from bank_account where branch_name in (select branch_name from branch where branch_city = 'Bombay');
-select * from bank_account;
+
+select distinct c.customer_name from bank_customer c,bank_account b where exists(select d.customer_name,count(d.customer_name) from depositer d,bank_account ba where ba.accno = d.accno and 
+c.customer_name = d.customer_name and ba.branch_name = 'SBI_ResidencyRoad' group by d.customer_name having count(d.customer_name)>=2);
+
+
+
